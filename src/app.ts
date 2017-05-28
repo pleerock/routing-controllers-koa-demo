4dee289b9040a88c9cd476ebe4e9def06f995427
Attila Olah <NoNameProvided@users.noreply.github.com>
2017-05-28 12:23:25 +0200

    docs: add code comments to explain the bootstrapping

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,36 @@
 import "reflect-metadata";
-import {useContainer, createKoaServer} from "routing-controllers";
-import {Container} from "typedi";
 
-// setup routing-controllers to use typedi container. You can use any container here
+import { createKoaServer, useContainer } from "routing-controllers";
+import { Container } from "typedi";
+
+/**
+ * Setup routing-controllers to use typedi container.
+ */
 useContainer(Container);
 
-// now import all our controllers. alternatively you can specify controllerDirs in routing-controller options
+/**
+ * We have to import all controllers, before creating the server.
+ * Alternatively we can specify a "controllers" option in
+ * createKoaServer/useKoaServer to load controllers from an array of paths.
+ */
 import "./controllers/CategoryController";
 import "./controllers/PostController";
 
-// create koa server
-const koaApp = createKoaServer({ // alternatively you can use useKoaServer with your own preconfigured koa server
-    // you also can do: controllerDirs: [__dirname + "/controllers"]
+/**
+ * We create a new koa server instance.
+ * We could have also use useKoaServer here to attach controllers to an
+ * existing koa instance.
+ */
+const koaApp = createKoaServer({
+    /**
+     * We can add options about how routing-controllers should
+     * configure itself.
+     */
 });
 
-// run koa app
+/**
+ * Start the koa app.
+ */
 koaApp.listen(3000);
 
 console.log("Server is up and running at port 3000");
